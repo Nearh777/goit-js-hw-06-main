5347f8b38ed9a757e4beca38276c3f03841d8ab7
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2023-04-21 13:59:29 +0000

    Deploying to gh-pages from @ Nearh777/goit-js-hw-06-main@a0975b4d51c1a19150eb192185e39006bba92e49 🚀

---
+++ task-02.578eedb5.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("#ingredients");console.log(o);const e=["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"].map((o=>{const e=document.createElement("li");return console.log(e),e.classList.add("item"),e.textContent=`${o}`,e}));console.log(e),o.append(...e);
+//# sourceMappingURL=task-02.578eedb5.js.map
