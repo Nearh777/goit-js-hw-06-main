be55f8b60e781ab3a14fd4fcaf9a178016a2d853
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2023-04-21 11:03:55 +0000

    Deploying to gh-pages from @ Nearh777/goit-js-hw-06-main@6066d09188c373fa9e6c27728e44881b43229e47 🚀

---
+++ task-01.21c88288.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".item");console.log("Кількість категорій:",e.length),e.forEach((e=>{const l=e.firstElementChild,o=e.lastElementChild.children;console.log("Категорія:",l.textContent),console.log("Елементів:",o.length)}));
+//# sourceMappingURL=task-01.21c88288.js.map
---
+++ task-01.a8a7e47f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll(".item");console.log("Кількість категорій:",e.length),e.forEach((function(e){var l=e.firstElementChild,n=e.lastElementChild.children;console.log("Категорія:",l.textContent),console.log("Елементів:",n.length)}))}();
+//# sourceMappingURL=task-01.a8a7e47f.js.map
